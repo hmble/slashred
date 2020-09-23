7235fcda447918f9b880bad5160075e7423d51ad
Samyak Bakliwal <w3bcode@gmail.com>
2020-09-23 10:51:40 +0530

    example: Add Create Multi example

---
+++ examples/create_multi/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/hmble/slashred"
+	_ "github.com/joho/godotenv/autoload"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+
+	authenticator := &slashred.Authenticator{
+		Config: &oauth2.Config{
+			ClientID:     os.Getenv("CLIENT_ID"),
+			ClientSecret: os.Getenv("CLIENT_SECRET"),
+			Scopes:       slashred.Scopes,
+			Endpoint:     slashred.Endpoint,
+			RedirectURL:  "https://example.com/auth",
+		},
+		Useragent: os.Getenv("USER_AGENT"),
+	}
+	u := &slashred.User{
+		Name:          "Samyak",
+		ProfileUrl:    "",
+		IsPermanent:   true,
+		Authenticator: authenticator,
+	}
+
+	token, er := slashred.TokenFromFile("token.json")
+	if er != nil {
+		log.Fatal("Error in reading token")
+	}
+
+	u.UpdateToken(token)
+
+	c := u.UserClient(token)
+
+	c.Multis.CreateMulti("astar0n", "test", `{
+    "description_md": "This is a test multi",
+    "display_name": "Test Multi",
+    "subreddits": [
+    {"name": "test"},
+    {"name": "vim"}
+    ],
+    "visibility": "private"
+  }`)
+
+}
